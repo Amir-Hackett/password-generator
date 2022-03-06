b09e6d9882f1b8d48f9a92648307f9846c1af97b
Amir <amir.hackett@gmail.com>
2022-03-05 19:35:31 -0500

    added new variables to array

--- Develop/script.js
+++ Develop/script.js
@@ -5,8 +5,8 @@ var generateBtn = document.querySelector("#generate");
 
 var arrayNumeric = ['0','1', '2', '3', '4','5', '6', '7', '8', '9']
 var arraySpecialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')',]
-var arrayUpper = ['A','B','C','D','E',]
-var arrayLow = ['a','b','c','d','e', ]
+var arrayUpper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+var arrayLow = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 function randomizeArray(array) {
 var selectIndex = Math.floor(Math.random() * array.length)
@@ -34,7 +34,7 @@ function userQuestions () {
 console.log(results)
   return results;
 }
-//console.log(generatePassword().results.confirmUppercase)
+
 function generatePassword(){
   var userResponse = userQuestions()
   var possilbleCharacters = []
@@ -67,6 +67,7 @@ for( var i = 0; i < userResponse.confirmCharacterQuanity; i++) {
 console.log(newPassword)
 return newPassword.join('')
 }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Password Generator Starter Code
 
 GIVEN I need a new, secure password
-WHEN I click the button to generate a password
+`WHEN I click the button to generate a password
 THEN I am presented with a series of prompts for password criteria
 WHEN prompted for password criteria
 THEN I select which criteria to include in the password
@@ -14,4 +14,4 @@ THEN my input should be validated and at least one character type should be sele
 WHEN all prompts are answered
 THEN a password is generated that matches the selected criteria
 WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
+THEN the password is either displayed in an alert or written to the page`
